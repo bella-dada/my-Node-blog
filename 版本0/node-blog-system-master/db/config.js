9a924a403572c3408182f383266648beeed8f306
liuqingxia <786540437@qq.com>
2022-04-18 20:22:29 +0800

    my-node-blog 第一次提交

---
+++ 版本0/node-blog-system-master/db/config.js
@@ -0,0 +1,12 @@
+let dbOption
+
+dbOption = {
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: '12345678',
+  port: '3306',
+  database: 'mynewblog'
+}
+
+module.exports = dbOption
