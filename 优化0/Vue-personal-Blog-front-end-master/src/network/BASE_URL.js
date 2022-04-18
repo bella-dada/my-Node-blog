5bb6dcbc2277f707ccbf8b3a3e4caf39de24a59c
liuqingxia <786540437@qq.com>
2022-04-18 20:22:47 +0800

    my-node-blog 第一次提交

--- 优化0/Vue-personal-Blog-front-end-master/src/network/BASE_URL.js
+++
@@ -1,5 +0,0 @@
-// 全部接口请求
-export const BASE_USERS_URL = '/api/users';
-export const BASE_SPEECH_URL = '/api/speech';
-export const BASE_ARTICLE_URL = '/api/article';
-export const BASE_COMMENT_URL = '/api/comment';
--- 优化0/node-blog-system-master/utils/constant.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // 处理密码的密钥
-    PWD_SALT:'xd_node',
-    // 处理token的密钥
-    PRIVATE_KEY:'xd_blog',
-    // token的过期时间
-    EXPIRESD:60*60*24
-}
